7ac62dc2b55b55292b954d8f0ff1b0331664f079
Christabelle <christefani@yahoo.com>
2023-07-18 09:50:26 +0000

    first commit

---
+++ conditionals.js
@@ -0,0 +1,21 @@
+let colleagues = ['Ben', 'Adam', 'Rachel', 'Jack', 'Debbie', 'Mara'];
+
+for ( let i=0; i<5; i++){
+console.log(colleagues[i]) 
+}
+
+
+let food = 'mashed potatoes and prawns'
+
+if (food == 'juice and toast') {
+    console.log('breakfast');
+
+} else if (food == 'spaghetti and meatballs') {
+    console.log('lunch');
+
+} else if (food == 'mashed potatoes and prawns') {
+    console.log('dinner');
+
+} else {
+    console.log ('snack');
+}
